28e89c0d478e5020d5e65030cbf81a5b571a40a8
rafazzaky <rafiazzakymc@gmail.com>
2022-12-08 13:48:32 +0700

    initial message

---
+++ script.js
@@ -0,0 +1,31 @@
+const head = document.getElementById("nav");
+let headHeight = head.offsetHeight;
+let yOffset = headHeight * -1 - 6;
+window.onresize = getHeadHeight;
+function getHeadHeight(){
+    headHeight = head.offsetHeight;
+    yOffset = headHeight * -1 - 6;
+    console.log(headHeight)
+;}
+
+function getHome(){
+    var element = document.getElementById("home");
+    scrollFixed(element);
+}
+function getDate(){
+    var element = document.getElementById("date");
+    scrollFixed(element);
+}
+function getArtist(){
+    var element = document.getElementById("artist");
+    scrollFixed(element);
+}
+function getPrice(){
+    var element = document.getElementById("price");
+    scrollFixed(element);
+}
+
+function scrollFixed(element){
+    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({top: y, behavior: 'smooth'});
+}
